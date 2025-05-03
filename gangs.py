eca7902e704946ce8822e0adf8a09b259127ec53
jake-yukich <jake.yukich@gmail.com>
2025-05-02 21:39:20 -0700

    start gangs

--- gangs.py
+++ gangs.py
@@ -1,12 +1,44 @@
 """TODO: playable gangs"""
 
-# class GangUnit:
-#     pass
+class Unit:
+    pass
 
-# class Solo(GangUnit):
+# class Solo(Unit):
 #     pass
 
 # # ... techies and other units...
 
-# class Gang:
-#     pass
+class Gang:
+     def __init__(self):
+          self.name = None
+
+          self.solos = []
+          self.techies = []
+          self.drones = []
+          self.netrunners = []
+
+          self.street_cred = 0
+          self.eurodollars = 0
+          self.contraband = 0
+          self.corporate_secrets = 0
+
+          self.actions = {
+               "available": [],
+               "reclaim": [],
+          }
+
+          self.starting_district = None
+          self.abilities = {}
+          self.hideouts = {}
+
+class TygerClaws(Gang):
+     pass
+
+class Valentinos(Gang):
+     pass
+
+class VoodooBoys(Gang):
+     pass
+
+class Maelstrom(Gang):
+     pass
