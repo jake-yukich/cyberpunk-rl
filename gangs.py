ca556785cf99270681fc9fc598311fe048b5df71
jake <jake.yukich@gmail.com>
2025-05-05 15:07:57 -0700

    add gangs, edgerunners (#5)

--- gangs.py
+++ gangs.py
@@ -1,16 +1,13 @@
 """TODO: playable gangs"""
 
-class Unit:
-    pass
-
-# class Solo(Unit):
-#     pass
-
-# # ... techies and other units...
+HEYWOOD = "Heywood"
+PACIFICA = "Pacifica"
+WATSON = "Watson"
+WESTBROOK = "Westbrook"
 
 class Gang:
-     def __init__(self):
-          self.name = None
+     def __init__(self, name: str):
+          self.name = name
 
           self.solos = []
           self.techies = []
@@ -27,18 +24,82 @@ class Gang:
                "reclaim": [],
           }
 
-          self.starting_district = None
           self.abilities = {}
           self.hideouts = {}
 
 class TygerClaws(Gang):
-     pass
+     def __init__(self):
+          super().__init__(name="Tyger Claws")
+          self.starting_district = WESTBROOK
+
+          self.actions["available"] = [
+               "Activate Solo",
+               "Activate Techies",
+               "Upgrade a Combat Card",
+               "Activate Netrunners",
+               "Build a Hideout",
+               "Action Wildcard",
+          ]
+
+          self.abilities = {
+               "REVENGE",
+               "PAYBACK",
+          }
 
 class Valentinos(Gang):
-     pass
+     def __init__(self):
+          super().__init__(name="Valentinos")
+          self.starting_district = HEYWOOD
+
+          self.actions["available"] = [
+               "Activate Solo",
+               "Activate Techies",
+               "Upgrade a Combat Card",
+               "Activate Netrunners",
+               "Build a Hideout",
+               "Action Wildcard",
+          ]
+
+          self.abilities = {
+               "COMMUNITY SUPPORT",
+               "PERSONAL OPPORTUNITY",
+          }
 
 class VoodooBoys(Gang):
-     pass
+     def __init__(self):
+          super().__init__(name="Voodoo Boys")
+          self.starting_district = PACIFICA
+
+          self.actions["available"] = [
+               "Activate Solo",
+               "Activate Techies",
+               "Upgrade a Combat Card",
+               "Activate Netrunners",
+               "Build a Hideout",
+               "Action Wildcard",
+          ]
+
+          self.abilities = {
+               "NETWATCH COUNTERMEASURES",
+               "MYSTIC NETRUNNERS",
+          }
 
 class Maelstrom(Gang):
-     pass
+     def __init__(self):
+          super().__init__(name="Maelstrom")
+          self.starting_district = WATSON
+
+          self.actions["available"] = [
+               "Activate Solo",
+               "Activate Techies",
+               "Upgrade a Combat Card",
+               "Activate Netrunners",
+               "Build a Hideout",
+               "Action Wildcard",
+          ]
+
+          self.abilities = {
+               "WELL-ARMED",
+               "RAGE",
+          }
+          self.rage_track = 0
