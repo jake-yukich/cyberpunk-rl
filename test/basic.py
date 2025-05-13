abbcb1817d6416a6aa4c5ee1f927b23b705ded9c
jake-yukich <jake.yukich@gmail.com>
2025-05-13 12:25:15 -0700

    init test

---
+++ test/basic.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestBasic(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_example(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
