a9acff1a778f83fe749334fb098b15124c194f70
jake-yukich <jake.yukich@gmail.com>
2025-05-14 16:50:32 -0700

    separate out district, markets, net

---
+++ game/markets.py
@@ -0,0 +1,15 @@
+class Market:
+    def __init__(self):
+        self.deck = []
+
+class CombatUpgradeMarket(Market):
+    def __init__(self):
+        super().__init__()
+
+class EdgerunnersMarket(Market):
+    def __init__(self):
+        super().__init__()
+
+class OpportunitiesMarket(Market):
+    def __init__(self):
+        super().__init__()
---
+++ game/net.py
@@ -0,0 +1,2 @@
+class TheNet:
+    pass
